c3290afd8638c8d6f0450b86fb2fbc8db6bda13c
maeng-kim <ehseo0410@gmail.com>
2024-01-03 02:17:11 +0900

    Initial Commit

---
+++ 백준/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  백준
+//
+//  Created by 김현수 on 1/3/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
