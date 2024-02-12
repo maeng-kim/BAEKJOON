005e39c44b1d157e1f43aa6939cb11d75d60c34e
maeng <89590797+maeng-kim@users.noreply.github.com>
2024-02-12 17:25:21 +0900

    [Silver IV] Title: 30, Time: 8 ms, Memory: 3044 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string num;
+    cin >> num;
+    
+    vector<int> digits;
+    int sum = 0;
+
+    for (int i = 0; i < num.size(); ++i) {
+        int digit = num[i] - '0';
+        digits.push_back(digit);
+        sum += digit;
+    }
+    sort(digits.begin(), digits.end(), greater<int>());
+    if (sum % 3 == 0 && digits[num.size()-1]==0) {
+        for (int i = 0; i < digits.size(); ++i) {
+            cout << digits[i];
+        }
+        //cout << "\n";
+    } else {
+        cout << -1;
+    }
+
+    return 0;
+}
